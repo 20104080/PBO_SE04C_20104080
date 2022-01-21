6b6958a3e7c4cafbf8c7136d1149477201a92dac
Aufar Bintang <82190084+20104080@users.noreply.github.com>
2022-01-21 13:24:36 +0700

    Add files via upload

---
+++ percobaan/Bank2.java
@@ -0,0 +1,8 @@
+package com.aufar.pbo.modul4.percobaan;
+
+//public class Bank2 {
+   // public static void main(String[] args) {
+     //   Tabungan saya = new Tabungan("Barca", 12345);
+       // Tabungan kamu = new Tabungan()
+  //  }
+//}
