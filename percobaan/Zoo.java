e0ca248ad803445dba1c64a4964eb32a8050ec94
Aufar Bintang <82190084+20104080@users.noreply.github.com>
2022-01-21 15:47:04 +0700

    Add files via upload

---
+++ percobaan/Zoo.java
@@ -0,0 +1,33 @@
+package com.aufar.pbo.modul6.percobaan;
+
+public class Zoo {
+    public static void main(String[] args) {
+        Animal lion = new Animal("Leon");
+        Animal unknown = new Animal();
+        Bird eagle =  new Bird("Birdy", 5);
+        Cat cat = new Cat("Miyu", 3, "Orange");
+
+        System.out.println("Lion name: " + lion.name);
+        System.out.println("Lion age : " + lion.age);
+        lion.walk();
+        System.out.println();
+
+        System.out.println("Unknown name: " + unknown.name);
+        System.out.println("Unknwon age : " + unknown.age);
+        unknown.walk();
+        System.out.println();
+
+        System.out.println("Bird name: " + eagle.name);
+        System.out.println("Bird age : " + eagle.age);
+        eagle.walk();
+        eagle.fly();
+        System.out.println();
+
+        System.out.println("Cat name: " + cat.name);
+        System.out.println("Cat age : " + cat.age);
+        System.out.println("Cat color: " + cat.color);
+        cat.walk();
+        System.out.println();
+
+    }
+}
