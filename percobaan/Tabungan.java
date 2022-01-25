2abb36d5ff560fe86c2b97f474c350a10c3a1c8e
Aufar Bintang <82190084+20104080@users.noreply.github.com>
2022-01-25 09:50:01 +0700

    Update Tabungan.java

--- percobaan/Tabungan.java
+++ percobaan/Tabungan.java
@@ -6,10 +6,33 @@ public class Tabungan {
     private int saldo;
     private int pin;
 
+    public Tabungan(String nama, int noRekening) {
+        this.nama = nama;
+        this.noRekening = noRekening;
+        this.saldo = 0;
+        this.pin = 12345;
+    }
+
     public Tabungan(String nama, int noRekening, int saldo, int pin) {
         this.nama = nama;
         this.noRekening = noRekening;
         this.saldo = saldo;
+        this.pin = pin;
+    }
+
+    public void simpanUang(int jumlah) {
+        this.saldo += jumlah;
+    }
+
+    public void tarikUang(int jumlah) {
+        this.saldo -= jumlah;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+
+    public void setPin(int pin) {
         this.pin = pin;
     }
 
